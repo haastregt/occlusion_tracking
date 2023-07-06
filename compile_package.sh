d598b393fb93d6848c496f17f5f1af36d2e4ca73
Truls Nyberg <truls.nyberg@gmail.com>
2023-07-06 10:40:11 +0200

    Removed private paths in compile_package.sh

--- compile_package.sh
+++ compile_package.sh
@@ -4,15 +4,17 @@ cd occlusion_package
 mkdir -p build
 mkdir -p install
 
+occlusion_package_path=$( pwd )
+echo Occlusion package path: $occlusion_package_path
 python_exec_path=$( which python3 )
 echo Python executable path: $python_exec_path
-PYTHONPATH="/home/jvarvn/.local/lib/python3.8/site-packages"
-echo Python library path: $PYTHONPATH
+python_site_packages_path=$( python3 -c "import sys; print(next(p for p in sys.path if 'site-packages' in p))" )
+echo Python library path: $python_site_packages_path
 
-sudo docker run -v /home/jvarvn/Documents/Implementations/occlusion_tracking/occlusion_package/build:/build             \
-                -v /home/jvarvn/Documents/Implementations/occlusion_tracking/occlusion_package/install:/install         \
-                -v /home/jvarvn/Documents/Implementations/occlusion_tracking/occlusion_package:/occlusion_package       \
+sudo docker run -v $occlusion_package_path/build:/build             \
+                -v $occlusion_package_path/install:/install         \
+                -v $occlusion_package_path:/occlusion_package       \
                 -t cgal_552                                                                                             \
                 bash -c "cmake ../occlusion_package -DPYTHON_LIBRARY_DIR=/install -DPYTHON_EXECUTABLE=$python_exec_path && make && make install"
 
-cp -a ./install/. $PYTHONPATH/
+cp -a ./install/. $python_site_packages_path/
